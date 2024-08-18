238c12bcb77b28683b555590dc0b4069e032d16e
Lu Jiazhou <2100012965@stu.pku.edu.cn>
2024-08-18 22:21:40 +0800

    use gin to make a RESTful server

--- wiki.go
+++ wiki.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 	"os"
-	"regexp"
-	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
+// use struct tags to conform to the json naming convention
 type Page struct {
-	Title string
-	Body  []byte
+	Title string `json:"title"`
+	Body  string `json:"body"`
 }
 
 func (p *Page) save() error {
 	filename := "pages/" + p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
+	return os.WriteFile(filename, []byte(p.Body), 0600)
 }
 
 func loadPage(title string) (*Page, error) {
@@ -26,92 +27,56 @@ func loadPage(title string) (*Page, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}, nil
+	return &Page{Title: title, Body: string(body)}, nil
 }
 
-var titlePattern = "[a-zA-Z0-9]+"
+func getPage(c *gin.Context) {
+	title := c.Param("title")
 
-var pageLink = regexp.MustCompile("[" + titlePattern + "]")
-var validPath = regexp.MustCompile("^/(edit|save|view)/(" + titlePattern + ")$")
-
-var templates = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html"))
-
-func replaceHyperLink(pattern []byte) []byte {
-	pageName := string(pattern[1 : len(pattern)-1])
-	re := fmt.Sprintf("<a href=%q>%v</a>", "/view/"+pageName, pageName)
-	return []byte(re)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page, replace bool) {
-	var renderedPage strings.Builder
-	err := templates.ExecuteTemplate(&renderedPage, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	renderedPageBytes := []byte(renderedPage.String())
-
-	// replacing the [PageName] with <a></a>
-	if replace {
-		renderedPageBytes = pageLink.ReplaceAllFunc(renderedPageBytes, replaceHyperLink)
-	}
-
-	fmt.Fprintf(w, "%s", renderedPageBytes)
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		c.JSON(http.StatusNotFound, gin.H{"message": "non-existing page"})
 		return
 	}
-	renderTemplate(w, "view", p, true)
+	c.JSON(http.StatusOK, p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p, false)
-}
+func savePage(c *gin.Context) {
+	var newPage Page
 
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if err := c.BindJSON(&newPage); err != nil {
+		fmt.Printf("%v", err)
 		return
 	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
 
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
-}
-
-const homePage = "/view/FrontPage"
-
-func homePageHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, homePage, http.StatusFound)
+	newPage.save()
+	c.JSON(http.StatusCreated, newPage)
 }
 
 func main() {
-	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	// p1.save()
-	// p2, _ := loadPage("TestPage")
-	// fmt.Println(string(p2.Body))
+	p1 := &Page{Title: "TestPage", Body: "This is a sample Page."}
+	p1.save()
+	p2, err := loadPage("TestPage")
+	if err != nil {
+		fmt.Printf("failed to create the example page: %e", err)
+		return
+	}
+	fmt.Println(p2.Body)
 
-	http.HandleFunc("/{$}", homePageHandler)
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := gin.Default()
+
+	router.Use(gin.Logger())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.GET("/view/:title", getPage)
+	router.POST("/save", savePage)
+
+	router.Run("localhost:30001")
 }
